12707e4fd9a7446ecd38f61f11e431a4d5ccad4f
walcutt <walcutt.0@gmail.com>
2022-07-05 23:43:28 -0400

    Add initial domain objects + test framework

---
+++ domain/distribution.js
@@ -0,0 +1,76 @@
+export const DEFAULT_CHARACTER = '';
+
+export class Distribution {
+    //Array<Pair<string, int>>
+    frequencyPDF;
+
+    //Array<Pair<string, int>>
+    frequencyCDF;
+
+    //int
+    cdfMax;
+
+    constructor(distSerialization = null) {
+        if(distSerialization === null || distSerialization.frequencies.length === 0) {
+            this.frequencyPDF = [];
+            this.frequencyCDF = [];
+            this.cdfMax = 0;
+            return;
+        }
+
+        this.frequencyPDF = distSerialization.frequencies.map(
+            e => {
+                return {
+                    char: e.char,
+                    count: e.count
+                };
+            }
+        );
+
+        let accumulator = 0;
+        this.frequencyCDF = [];
+        for(let i = 0; i < this.frequencyPDF.length; i++) {
+            accumulator += this.frequencyPDF[i].count;
+            this.frequencyCDF[i] = {
+                char: this.frequencyPDF[i].char,
+                count: accumulator
+            };
+        }
+        this.cdfMax = accumulator;
+    }
+
+    getValFromCDF(linearParam) {
+        let totalParam = linearParam * this.cdfMax;
+        if(totalParam < 0) totalParam = 0;
+        if(totalParam > this.cdfMax) totalParam = this.cdfMax;
+        
+        for(let i = 0; i < this.frequencyCDF.length; i++) {
+            if(this.frequencyCDF[i].count >= totalParam) {
+                return this.frequencyCDF[i].char;
+            }
+        }
+
+        //In null dist case (pdf = cdf = []), should pass over loop to here
+        return DEFAULT_CHARACTER;
+    }
+
+    serialize() {
+        return new DistributionSerialization(this.frequencyPDF);
+    }
+}
+
+export class DistributionSerialization {
+    //Array<Pair<string, int>>
+    frequencies;
+
+    constructor(frequencyDist = []) {
+        this.frequencies = frequencyDist.map(
+            e => {
+                return {
+                    char: e.char,
+                    count: e.count
+                };
+            }
+        );
+    }
+}
---
+++ tests/domain/distribution.test.js
@@ -0,0 +1,87 @@
+import { Distribution, DistributionSerialization, DEFAULT_CHARACTER } from '../../domain/distribution.js';
+
+test('Default Params', () => {
+    let dist = new Distribution();
+    let distSer = new DistributionSerialization();
+
+    expect(dist.frequencyPDF).toEqual([]);
+    expect(dist.frequencyCDF).toEqual([]);
+    expect(dist.cdfMax).toBe(0);
+
+    expect(distSer.frequencies).toEqual([]);
+});
+
+test('Null distribution generator', () => {
+    let dist = new Distribution();
+
+    expect(dist.getValFromCDF(0)).toEqual(DEFAULT_CHARACTER);
+    expect(dist.getValFromCDF(0.5)).toEqual(DEFAULT_CHARACTER);
+    expect(dist.getValFromCDF(1)).toEqual(DEFAULT_CHARACTER);
+});
+
+test('Preservation of data structure through conversions', () => {
+    let distributionSer = new DistributionSerialization([
+        {
+            char: 'a',
+            count: 1
+        },
+        {
+            char: 'b',
+            count: 2
+        },
+        {
+            char: 'c',
+            count: 3
+        }
+    ]);
+    let distribution = new Distribution(distributionSer);
+    let ds2 = distribution.serialize();
+    let d2 = new Distribution(ds2);
+
+    expect(distributionSer).toEqual(ds2);
+    expect(distribution).toEqual(d2);
+});
+
+test('Non-null random generation within bounds', () => {
+    let distributionSer = new DistributionSerialization([
+        {
+            char: 'a',
+            count: 1
+        },
+        {
+            char: 'b',
+            count: 2
+        },
+        {
+            char: 'c',
+            count: 3
+        }
+    ]);
+    let dist = new Distribution(distributionSer);
+
+    expect(dist.getValFromCDF(0)).toEqual('a');
+    expect(dist.getValFromCDF(0.5 / 6)).toEqual('a');
+    expect(dist.getValFromCDF(2 / 6)).toEqual('b');
+    expect(dist.getValFromCDF(4 / 6)).toEqual('c');
+});
+
+test('Non-null random generation out of bounds', () => {
+    let distributionSer = new DistributionSerialization([
+        {
+            char: 'a',
+            count: 1
+        },
+        {
+            char: 'b',
+            count: 2
+        },
+        {
+            char: 'c',
+            count: 3
+        }
+    ]);
+    let dist = new Distribution(distributionSer);
+
+    expect(dist.getValFromCDF(-1)).toEqual('a');
+    expect(dist.getValFromCDF(2)).toEqual('c');
+});
