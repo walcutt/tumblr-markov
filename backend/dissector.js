405b5182ab8d5738a0e94e33d728fed04cd656d6
walcutt <walcutt.0@gmail.com>
2022-07-06 22:50:27 -0400

    Added service to convert text to markov matrix

---
+++ backend/dissector.js
@@ -0,0 +1,38 @@
+import { DEFAULT_CHARACTER } from '../domain/distribution.js';
+import { MarkovMatrix, MarkovMatrixSerialization } from '../domain/matrix.js';
+
+export class Dissector {
+    //MarkovMatrix
+    matrix;
+
+    //int
+    prefixDepth;
+
+    constructor(prefixDepth, markovMatrixSerialization = null) {
+        this.matrix = new MarkovMatrix(
+            markovMatrixSerialization ?? new MarkovMatrixSerialization()
+        );
+        this.prefixDepth = prefixDepth;
+    }
+
+    dissectText(text) {
+        // First, generate using prefixes that are smaller than @prefixDepth
+        for(let i = 0; i < this.prefixDepth && i < text.length; i++) {
+            let prefix = text.substring(0, i);
+            this.matrix.registerPair(prefix, text.charAt(i));
+        }
+
+        // Then, generate using remaining prefixes of length prefixDepth.
+        for(let k = 0; k < text.length - this.prefixDepth; k++) {
+            let prefix = text.substring(k, k + this.prefixDepth);
+            this.matrix.registerPair(prefix, text.charAt(k + this.prefixDepth));
+        }
+
+        // Finally, register last prefix.
+        let prefix = text.substring(
+            Math.max(text.length - this.prefixDepth, 0),
+            text.length
+        );
+        this.matrix.registerPair(prefix, DEFAULT_CHARACTER);
+    }
+}
---
+++ tests/backend/dissector.test.js
@@ -0,0 +1,303 @@
+import { Dissector } from '../../backend/dissector.js';
+import { DEFAULT_CHARACTER, Distribution, DistributionSerialization } from '../../domain/distribution';
+import { MarkovMatrix, MarkovMatrixSerialization } from '../../domain/matrix.js';
+
+test('Test default parameters', () => {
+    let dissector = new Dissector(1);
+
+    let matrix = new MarkovMatrix();
+
+    expect(dissector.matrix).toEqual(matrix);
+});
+
+test('Non-null matrix parameter', () => {
+    let dist = new Distribution(
+        new DistributionSerialization(
+            [
+                {
+                    char: 'a',
+                    count: 1
+                }
+            ]
+        )
+    );
+    let matrixSerialization = new MarkovMatrixSerialization(
+        {
+            a: dist
+        },
+        [ 'a' ]
+    );
+
+    let dissector = new Dissector(1, matrixSerialization);
+
+    expect(dissector.matrix.serialize()).toEqual(matrixSerialization);
+});
+
+test('Empty string dissection', () => {
+    let dist = new Distribution(
+        new DistributionSerialization(
+            [
+                {
+                    char: DEFAULT_CHARACTER,
+                    count: 1
+                }
+            ]
+        )
+    );
+    let mms = new MarkovMatrixSerialization(
+        {
+            '': dist
+        },
+        [ '' ]
+    );
+    let dissector = new Dissector(1);
+
+    dissector.dissectText('');
+
+    expect(dissector.matrix.serialize()).toEqual(mms);
+});
+
+test('Nominal string dissection', () => {
+    let mms = new MarkovMatrixSerialization(
+        {
+            '': new Distribution(
+                new DistributionSerialization(
+                    [
+                        {
+                            char: 'a',
+                            count: 1
+                        }
+                    ]
+                )
+            ),
+            'a': new Distribution(
+                new DistributionSerialization(
+                    [
+                        {
+                            char: 'b',
+                            count: 1
+                        }
+                    ]
+                )
+            ),
+            'ab': new Distribution(
+                new DistributionSerialization(
+                    [
+                        {
+                            char: 'c',
+                            count: 1
+                        }
+                    ]
+                )
+            ),
+            'bc': new Distribution(
+                new DistributionSerialization(
+                    [
+                        {  
+                            char: DEFAULT_CHARACTER,
+                            count: 1
+                        }
+                    ]
+                )
+            )
+        },
+        [ '', 'a', 'ab', 'bc' ]
+    );
+
+    let dissector = new Dissector(2);
+    dissector.dissectText('abc');
+
+    expect(dissector.matrix.serialize()).toEqual(mms);
+});
+
+test('Repeating prefix string dissection', () => {
+    let mms = new MarkovMatrixSerialization(
+        {
+            '': new Distribution(
+                new DistributionSerialization(
+                    [
+                        {
+                            char: 'a',
+                            count: 1
+                        }
+                    ]
+                )
+            ),
+            'a': new Distribution(
+                new DistributionSerialization(
+                    [
+                        {
+                            char: 'a',
+                            count: 1
+                        }
+                    ]
+                )
+            ),
+            'aa': new Distribution(
+                new DistributionSerialization(
+                    [
+                        {
+                            char: 'a',
+                            count: 1
+                        },
+                        {
+                            char: 'b',
+                            count: 1
+                        }
+                    ]
+                )
+            ),
+            'ab': new Distribution(
+                new DistributionSerialization(
+                    [
+                        {
+                            char: 'c',
+                            count: 1
+                        }
+                    ]
+                )
+            ),
+            'bc': new Distribution(
+                new DistributionSerialization(
+                    [
+                        {  
+                            char: DEFAULT_CHARACTER,
+                            count: 1
+                        }
+                    ]
+                )
+            )
+        },
+        [ '', 'a', 'aa', 'ab', 'bc' ]
+    );
+
+    let dissector = new Dissector(2);
+    dissector.dissectText('aaabc');
+
+    expect(dissector.matrix.serialize()).toEqual(mms);
+});
+
+test('Dissecting multiple strings', () => {
+    let mms = new MarkovMatrixSerialization(
+        {
+            '': new Distribution(
+                new DistributionSerialization(
+                    [
+                        {
+                            char: 'a',
+                            count: 2
+                        }
+                    ]
+                )
+            ),
+            'a': new Distribution(
+                new DistributionSerialization(
+                    [
+                        {
+                            char: 'a',
+                            count: 1
+                        },
+                        {
+                            char: 'b',
+                            count: 1
+                        }
+                    ]
+                )
+            ),
+            'aa': new Distribution(
+                new DistributionSerialization(
+                    [
+                        {
+                            char: 'a',
+                            count: 1
+                        },
+                        {
+                            char: 'b',
+                            count: 1
+                        }
+                    ]
+                )
+            ),
+            'ab': new Distribution(
+                new DistributionSerialization(
+                    [
+                        {
+                            char: 'c',
+                            count: 2
+                        }
+                    ]
+                )
+            ),
+            'bc': new Distribution(
+                new DistributionSerialization(
+                    [
+                        {  
+                            char: DEFAULT_CHARACTER,
+                            count: 2
+                        }
+                    ]
+                )
+            )
+        },
+        [ '', 'a', 'aa', 'ab', 'bc' ]
+    );
+
+    let dissector = new Dissector(2);
+    dissector.dissectText('aaabc');
+    dissector.dissectText('abc');
+
+    expect(dissector.matrix.serialize()).toEqual(mms);
+});
+
+test('Prefix longer than string', () => {
+    let mms = new MarkovMatrixSerialization(
+        {
+            '': new Distribution(
+                new DistributionSerialization(
+                    [
+                        {
+                            char: 'a',
+                            count: 1
+                        }
+                    ]
+                )
+            ),
+            'a': new Distribution(
+                new DistributionSerialization(
+                    [
+                        {
+                            char: 'b',
+                            count: 1
+                        }
+                    ]
+                )
+            ),
+            'ab': new Distribution(
+                new DistributionSerialization(
+                    [
+                        {
+                            char: 'c',
+                            count: 1
+                        }
+                    ]
+                )
+            ),
+            'abc': new Distribution(
+                new DistributionSerialization(
+                    [
+                        {  
+                            char: DEFAULT_CHARACTER,
+                            count: 1
+                        }
+                    ]
+                )
+            )
+        },
+        [ '', 'a', 'ab', 'abc' ]
+    );
+
+    let dissector = new Dissector(4);
+    dissector.dissectText('abc');
+
+    expect(dissector.matrix.serialize()).toEqual(mms);
+});
